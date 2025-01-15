06730f941f7cc7730a65049328ee5a888b3cd947
Queue-Evgenii <genadolgii228@gmail.com>
2025-01-15 09:46:49 +0200

    magnetyczne

--- dist/assets/index.8bc79ef3.js
+++
@@ -1,1 +0,0 @@
-const e={id:3,parent_dir:"oswietlenie-szynowe-magnetyczne",name:"O\u015Bwietlenie szynowe magnetycznego",data:{type:"file",data:"KATALOG TREK MAGNETYCZNY.pdf"}};export{e as data};
--- src/data/categories/oswietlenie-szynowe-magnetyczne/index.js
+++ src/data/categories/oswietlenie-szynowe-magnetyczne/index.js
@@ -1,7 +1,7 @@
 export const data = {
   id: 3,
   parent_dir: "oswietlenie-szynowe-magnetyczne",
-  name: "Oświetlenie szynowe magnetycznego",
+  name: "Oświetlenie szynowe magnetyczne",
   data: {
     type: "file",
     data: "KATALOG TREK MAGNETYCZNY.pdf",
