5691c39ca2541292da209d6e8e3a108f1ed8b51a
sundevil <sundevildi@gmail.com>
2025-10-06 12:44:22 +0200

    upd 3 pdf

---
+++ dist/assets/index.0b34d477.js
@@ -0,0 +1,1 @@
+const a={id:1,parent_dir:"katalog-naraznicov",name:"Katalog narozniki la\u0328czniki.",data:{type:"file",data:"Katalog narozniki la\u0328czniki.pdf"}};export{a as data};
---
+++ src/data/categories/katalog-naraznicov/index.js
@@ -0,0 +1,9 @@
+export const data = {
+  id: 1,
+  parent_dir: "katalog-naraznicov",
+  name: "Katalog narozniki lączniki.",
+  data: {
+    type: "file",
+    data: "Katalog narozniki lączniki.pdf",
+  },
+};
